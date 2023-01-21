372633b09e78a34e665323e61381130e57d44947
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-01-21 20:31:42 +0200

    Add util.h .c file will contain util functions, struct ... Begin to implement quick sort

---
+++ util.h
@@ -0,0 +1,12 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void _qsort(void* v, int size, int left, int right, int (*comp)(void*, void*));
+void swap(void* v1, void* v2, int size);
+
+#endif
